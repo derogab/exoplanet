23d8649322ad1826c107e06346069b4c3083bdf3
Gabriele De Rosa <derosagabriele@outlook.it>
2021-01-18 16:37:37 +0100

    More about PCA
    Co-authored-by: dlcgold <dlcgold96@gmail.com>
    Co-authored-by: fdila <federicadilauro1998@gmail.com>

--- src/pca.r
+++ src/pca.r
@@ -66,23 +66,29 @@ data$koi_disposition <- as.factor(data$koi_disposition)
 
 # Clear row with n/a
 data <- data[complete.cases(data),]
-
+cat("Number of features: ", ncol(data),"\n")
 # Plot distribution of target before downsample
+png(filename="outputs/before_downsample_distr.pca.png")
 plot(data$koi_disposition)
+garbage <- dev.off()
 
 # Downsample the data based on target (koi_disposition)
 data <- downsample(data, cat_col="koi_disposition")
 
 # Plot distribution of target after downsample 
+png(filename="outputs/after_downsample_distr.pca.png")
 plot(data$koi_disposition)
+garbage <- dev.off()
 
 # Remove label from train set
 data.active <- subset(data, select=-c(koi_disposition))
 
 # Correlation matrix
-M<-cor(data.active, use="pairwise.complete.obs")
-head(round(M,2))
-corrplot(M)
+#M<-cor(data.active, use="pairwise.complete.obs")
+#head(round(M,2))
+#png(filename="outputs/corr_matrix.pca.png")
+#corrplot(M)
+#dev.off()
 
 # Execute PCA
 res.pca <- PCA(data.active, 
@@ -92,10 +98,32 @@ res.pca <- PCA(data.active,
 
 # Extract eigenvalues and graph
 eig.val <- get_eigenvalue(res.pca)
+
+
+png(filename="outputs/cumulative_variance.pca.png")
+plot(eig.val[,3], type="l", xlab="Dimensions", ylab="Cumulative Variance")
+garbage <- dev.off()
+
 eig.graph = fviz_eig(res.pca, 
                      addlabels=TRUE, 
                      ncp=ncol(data.active))
-                     
+png(filename="outputs/distr.pca.png", width = 1480, height = 550)                 
+eig.graph
+garbage <- dev.off()
+#png(filename="outputs/pca_distr_test.png", width = 1480, height = 550)
+#fviz_pca_var(res.pca, col.var = "black")
+#dev.off()
+
+#png(filename="outputs/pca_distr_lol.png", width = 1480, height = 550)                 
+#fviz_eig(res.pca,
+#         addlabels = T, 
+#         barcolor = "#E7B800", 
+#         barfill = "#E7B800", 
+#         linecolor = "#00AFBB", 
+#         choice = "variance", 
+#         ylim=c(0,10))
+#dev.off()
+
 
 # Get PCA variance
 var <- get_pca_var(res.pca)
@@ -103,22 +131,32 @@ var <- get_pca_var(res.pca)
 # Corrplot
 # The contributions of variables in accounting for the variability in a 
 # given principal component are expressed in percentage
-corrplot(cor(var$contrib))   
+#png(filename="outputs/corr_matrix_after_pca.png")
+#corrplot(cor(var$contrib), is.corr=FALSE)   
+#dev.off()
 
-# Count number of dimensions with eigenvalue >= 1
+# Count number of dimensions with eigenvalue > 1
 count_eig <- 0
 for(i in 1:nrow(eig.val)){
-  if(eig.val[i]>= 1){
+  if(eig.val[i] > 1){
     count_eig <- count_eig + 1
   }
 }
 
-# Contributions of variables to principal components
-fviz_contrib(res.pca, choice = "var", axes = 1:count_eig, top = 40)
+# Print the component with eig.val > 1
+cat('Count of component with eig.val > 1: ', count_eig, '\n')
 
+# Print the cumulative variance at last component with eig.val > 1
+cat('Cumulative variance at last component with eig.val > 1: ', eig.val[count_eig, 3], '%\n')
+
+
+
+# Contributions of variables to principal components
+png(filename="outputs/hist_pca.png", width = 1480, height = 550)
+fviz_contrib(res.pca, choice = "var", axes = 1:count_eig, title = "")
+garbage <- dev.off()
 # Get first count_eig contributions of individuals
 var_contrib = head(var$contrib, count_eig)
-
 # Get the names of significative attributes
 attributes <- rownames(var_contrib)
 
@@ -140,12 +178,16 @@ train = all$train # get train set
 test = all$test # get test set
 
 # Plot train and test distribution of target
+png(filename="outputs/train_distr.pca.png")
 plot(train$koi_disposition)
+garbage <- dev.off()
+png(filename="outputs/test_distr.pca.png")
 plot(test$koi_disposition)
+garbage <- dev.off()
 
 # Get PCA train and test
-train.pca = select(train, attributes)
-test.pca = select(test, attributes)
+train.pca = select(train, all_of(attributes))
+test.pca = select(test, all_of(attributes))
 
 # Write csv files
 write.csv(train, "datasets/tmp/train.csv", row.names = FALSE)
